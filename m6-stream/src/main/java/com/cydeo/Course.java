2a4a9f92c24ad9ef822a0fd95ac1cb235fd7285a
majastamenkovic <majastam1996@icloud.com>
2024-03-10 19:05:13 +0100

    Stream

---
+++ m6-stream/src/main/java/com/cydeo/Course.java
@@ -0,0 +1,14 @@
+package com.cydeo;
+
+public class Course {
+    private String coursename;
+    private int courseId;
+
+    public Course(String coursename, int courseId) {
+        this.coursename = coursename;
+        this.courseId = courseId;
+    }
+
+
+}
+
