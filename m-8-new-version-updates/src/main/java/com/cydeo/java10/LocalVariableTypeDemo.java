ad30bf8099194481cef06d2b91a25f5433af8f00
majastamenkovic <majastam1996@icloud.com>
2024-03-24 18:29:48 +0100

    java10

---
+++ m-8-new-version-updates/src/main/java/com/cydeo/java10/LocalVariableTypeDemo.java
@@ -0,0 +1,22 @@
+package com.cydeo.java10;
+
+
+public class LocalVariableTypeDemo {
+    public static void main(String[] args) {
+
+        //Type Inference
+        String s;
+        s= "Ozzy";
+        var str= "Cydeo";
+
+        var number = 5;
+
+        byte x = 1;
+        var y = 1; //int
+
+        var price = 12.50;
+
+
+
+    }
+}
